f4e82a63217192f167012ba6aece1f572ff250d2
QueenKaufi <90047853+QueenKaufi@users.noreply.github.com>
2021-09-07 01:09:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var principal = document.getElementById("principal");
+var rate = document.getElementById("rate");
+var years = document.getElementById("years");
+
+var result = document.getElementById("result")
+
+var slider= document.getElementById("rate");
+var output= document.getElementById("rate_val");
+
+
+function updateRate(){
+output.innerHTML=slider.value + "%"
+	 }
+   
+function compute(){
+
+var interest = principal.value * years.value * rate.value /100;
+var futureyear = new Date().getFullYear()+parseInt(years.value);
+
+if(principal.value <= 0){
+        alert("Enter a positive number")
+        principal.focus()
+        return}
+  result.innerHTML="If you deposit <mark>"+principal.value+"</mark>,\<br\>at an interest rate of <mark>"+rate.value+"</mark>%.\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+futureyear+"</mark>\<br\>"
 }
-        
